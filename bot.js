f6c2d474942f0bd39571a55830161ec0bc13f4c9
Pablo Gutierrez <pablogtzgileta@gmail.com>
2019-09-24 16:52:24 -0500

    Modified initial files

--- bot.js
+++ bot.js
@@ -1,19 +1,30 @@
-class EchoBot {
-    async onTurn(context) {
-        // Check to see if this activity is an incoming message.
-        // (It could theoretically be another type of activity.)
-        if (context.activity.type === 'message' && context.activity.text) {
-            // Check to see if the user sent a simple "quit" message.
-            if (context.activity.text.toLowerCase() === 'quit') {
-                // Send a reply.
-                context.sendActivity(`Bye!`);
-                process.exit();
-            } else {
-                // Echo the message text back to the user.
-                return context.sendActivity(`I heard you say "${ context.activity.text }"`);
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler } = require('botbuilder');
+
+class MyBot extends ActivityHandler {
+    constructor() {
+        super();
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onMessage(async (context, next) => {
+            await context.sendActivity(`You said '${ context.activity.text }'`);
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity('Hello and welcome!');
+                }
             }
-        }
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
     }
+
 }
 
-module.exports.EchoBot = EchoBot;
+module.exports.MyBot = MyBot;
--- index.js
+++ index.js
@@ -1,25 +1,51 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const dotenv = require('dotenv');
 const path = require('path');
-const { ConsoleAdapter } = require('./consoleAdapter');
+const restify = require('restify');
 
-// load environment variables from .env file.
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const { BotFrameworkAdapter } = require('botbuilder');
+
+// This bot's main dialog.
+const { MyBot } = require('./bot');
+
+// Import required bot configuration.
 const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
+dotenv.config({ path: ENV_FILE });
 
-// Create the bot adapter, which is responsible for sending and receiving messages.
-// We are using the ConsoleAdapter, which enables a bot you can chat with from within your terminal window.
-const adapter = new ConsoleAdapter();
-
-// Import our bot class.
-const { EchoBot } = require('./bot');
-const bot = new EchoBot();
-
-// A call to adapter.listen tells the adapter to start listening for incoming messages and events, known as "activities."
-// Activities are received as TurnContext objects by the handler function.
-adapter.listen(async (context) => {
-    bot.onTurn(context);
+// Create HTTP server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo test your bot, see: https://aka.ms/debug-with-emulator`);
 });
 
-// Emit a startup message with some instructions.
-console.log('> Console EchoBot is online. I will repeat any message you send me!');
-console.log('> Say "quit" to end.');
-console.log(''); // Leave a blank line after instructions.
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Catch-all for errors.
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    console.error(`\n [onTurnError]: ${ error }`);
+    // Send a message to the user
+    await context.sendActivity(`Oops. Something went wrong!`);
+};
+
+// Create the main dialog.
+const myBot = new MyBot();
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route to main dialog.
+        await myBot.run(context);
+    });
+});
